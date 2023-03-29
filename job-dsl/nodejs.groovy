32711fbf82ed0b4e5b055d8fcb197ca8be775eb0
sonys207 <jichen.liu85@gmail.com>
2023-03-29 12:07:27 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,9 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git')
+            git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
     }
     triggers {
         scm('H/5 * * * *')
