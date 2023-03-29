6234f1b149d2189e9c57976be5a3ac1500602c0b
sonys207 <jichen.liu85@gmail.com>
2023-03-29 17:51:17 -0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -9,7 +9,7 @@ job('NodeJS-Docker-example') {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs19.8.1') // this is the name of the NodeJS installation in 
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
