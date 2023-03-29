dd4741058e8c0e21ce1eb05036b53000b84d644c
sonys207 <jichen.liu85@gmail.com>
2023-03-29 17:31:09 -0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS-Docker-example') {
         dockerBuildAndPublish {
             repositoryName('tonytest2021/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            dockerRegistryURL('https://index.docker.io/v1/')
+            dockerRegistryURL('https://registry.hub.docker.com/')
             registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
